d8345ed18087b89d3b8e07fba57981540982fbaf
Johannes Skov Frandsen <localgod@heaven.dk>
2020-07-14 15:24:20 +0200

    Updated publish

--- publish.sh
+++ publish.sh
@@ -1,13 +1,26 @@
-TARGET_BRANCH=master
-if [ "${GITHUB_EVENT_NAME}" == "push" ]; then
-    cd ${HOME}
-    git config --global user.email "actions@github.com"
-    git config --global user.name "Git Actions"
-    git clone --quiet --branch=${TARGET_BRANCH} https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git ${TARGET_BRANCH} > /dev/null
-    cd master
-    rm -rf *.*
-    cp -Rf ${HOME}/dist ./
-    git add -f .
-    git commit -m "Github Actions build ${GITHUB_RUN_NUMBER} pushed to master"
-    git push -fq origin ${TARGET_BRANCH} > /dev/null
+#!/usr/bin/env bash
+
+TARGET_BRANCH=${1:-"master"}
+EVENT_NAME=${2:-${GITHUB_EVENT_NAME}}
+TOKEN=${3:-${GITHUB_TOKEN}}
+REPO=${4:-${GITHUB_REPOSITORY}}
+RUN=${5:-${GITHUB_RUN_NUMBER}}
+
+if [ "${EVENT_NAME}" == "push" ]; then
+  working_dir=$(pwd)
+  org_name="$(git config user.email)"
+  org_email="$(git config user.name)"
+  git config user.email "actions@github.com"
+  git config user.name "Git Actions"
+  git clone --quiet --branch=${TARGET_BRANCH} https://${TOKEN}@github.com/${REPO}.git ${TARGET_BRANCH} > /dev/null
+  cd ${TARGET_BRANCH}
+  ls -la
+  rm -rf *.*
+  cp -Rf ${working_dir}/dist ./
+  #git add -f .
+  #git commit -m "Github Actions build ${GITHUB_RUN_NUMBER} pushed to master"
+  #git push -fq origin ${TARGET_BRANCH} > /dev/null
+  cd ..
+  git config user.email "${org_email}"
+  git config user.name "${org_name}"
 fi
